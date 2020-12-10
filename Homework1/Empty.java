92260d34881911f5aa2daeda273a04ea4daa78fa
badamson1999 <61796259+badamson1999@users.noreply.github.com>
2020-12-10 15:59:06 -0600

    Create Empty.java

---
+++ Homework1/Empty.java
@@ -0,0 +1,51 @@
+package edu.iastate.cs228.hw1;
+
+/**
+ *
+ * @author Bryanna Adamson
+ *
+ */
+
+/**
+ * Empty squares are competed by various forms of life.
+ */
+public class Empty extends Living {
+
+	public Empty (Plain p, int r, int c) {
+		super.row = r;
+		super.column = c;
+		super.plain =p;
+
+	}
+	public State who(){
+			return State.EMPTY;
+	}
+
+	/**
+	 * An empty square will be occupied by a neighboring Badger, Fox, Rabbit, or Grass, or remain empty.
+	 * @param pNew     plain of the next life cycle.
+	 * @return Living  life form in the next cycle.
+	 */
+	public Living next(Plain pNew){
+		int[] pop = new int[NUM_LIFE_FORMS];
+		census(pop);
+		if(pop[RABBIT]>1) {
+			return new Rabbit(pNew,row,column,0);
+		}
+		else
+			if(pop[FOX]>1) {
+				return new Fox(pNew,row,column,0);
+			}
+			else
+				if(pop[BADGER]>1) {
+					return new Badger(pNew,row,column,0);
+				}
+				else
+					if(pop[GRASS]>=1) {
+						return new Grass(pNew,row,column);
+					}
+					else {
+						return new Empty(pNew,row,column);
+					}
+	}
+}
