a2a0ade08ff4ec8fc81f403f2c0b4b6523b53c27
badamson1999 <61796259+badamson1999@users.noreply.github.com>
2020-12-10 15:58:47 -0600

    Create Rabbit.java

---
+++ Homework1/Rabbit.java
@@ -0,0 +1,59 @@
+package edu.iastate.cs228.hw1;
+
+/**
+ *
+ * @author Bryanna Adamson
+ *
+ */
+
+/*
+ * A rabbit eats grass and lives no more than three years.
+ */
+public class Rabbit extends Animal {
+	/**
+	 * Creates a Rabbit object.
+	 * @param p: plain
+	 * @param r: row position
+	 * @param c: column position
+	 * @param a: age
+	 */
+	public Rabbit (Plain p, int r, int c, int a) {
+		super.row = r;
+		super.column = c;
+		super.age =a;
+		super.plain =p;
+	}
+
+	// Rabbit occupies the square.
+	public State who(){
+		return State.RABBIT;
+	}
+
+	/**
+	 * A rabbit dies of old age or hunger. It may also be eaten by a badger or a fox.
+	 * @param pNew     plain of the next cycle
+	 * @return Living  new life form occupying the same square
+	 */
+	public Living next(Plain pNew){
+		int[] pop = new int[NUM_LIFE_FORMS];
+		census(pop);
+		if(age == 3) {
+			return new Empty(pNew,row,column);
+		}
+		else
+			if(pop[GRASS] ==0) {
+				return new Empty(pNew,row,column);
+			}
+			else
+				if(pop[BADGER]+pop[FOX]>=pop[RABBIT] && pop[FOX]>pop[BADGER]){
+					return new Fox(pNew,row,column,0);
+				}
+				else
+					if(pop[BADGER]>pop[RABBIT]){
+						return new Badger(pNew,row,column,0);
+				}
+					else {
+						return new Rabbit(pNew,row,column,age);
+					}
+	}
+}
